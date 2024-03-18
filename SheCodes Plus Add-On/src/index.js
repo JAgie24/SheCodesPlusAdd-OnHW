6bc5a9fa45c1944022fe61f929ecfd99b77d42a3
JAgie24 <misso_joseph@hotmail.ca>
2024-03-18 03:20:17 -0400

    cleaned code based on solution

--- SheCodes Plus Add-On/src/index.js
+++ SheCodes Plus Add-On/src/index.js
@@ -1,23 +1,11 @@
 function aletDate(event) {
-  let tokyoDate = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
-  let parisDate = moment()
-    .tz("France/Paris")
-    .format("dddd, MMMM D, YYYY h:mm A");
-  let sydneyDate = moment()
-    .tz("Austrailia/Sydney")
-    .format("dddd, MMMM D, YYYY h:mm A");
   if (event.target.value.length > 0) {
-    if (event.target.value === "tokyo") {
-      alert`It is ${tokyoDate} in Aisa/Tokyo`;
-    }
-    if (event.target.value === "paris") {
-      alert`It is ${parisDate} in Europe/Paris`;
-    }
-    if (event.target.value === "sydney") {
-      alert`It is ${sydneyDate} in Austrailia/Sydney`;
-    }
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 
-let country = document.querySelector("#county");
-country.addEventListener("change", aletDate);
+let city = document.querySelector("#city");
+city.addEventListener("change", aletDate);
